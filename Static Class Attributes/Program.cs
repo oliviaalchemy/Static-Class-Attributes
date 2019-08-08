c65f53b41e7ac96fde712d18627877b00bd1f99f
oliviaalchemy <olivia@alchemytechs.com>
2019-08-08 14:42:10 +0100

    Add project files.

---
+++ Static Class Attributes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Static_Class_Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(kashmir.getSongCount()); 
+           
+
+            Console.ReadLine(); 
+        }
+    }
+}
